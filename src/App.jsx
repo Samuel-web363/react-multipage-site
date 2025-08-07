d7da7edbd0e9243411f5acfba8804225429aa833
Samuel-web363 <chinonsosamuel2654@gmail.com>
2025-08-07 08:19:06 -1100

    Updated layout and fixed footer design

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import {Routes, Route } from "react-router-dom";
-import "./App.css";
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import Contact from "./Pages/Contact";
-import Navigation from "./Components/Navigation";
-import Footer from "./Components/Footer";
+import Navigation from './Components/Navigation';
+import Footer from './Components/Footer';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-      <div className="App">
-        <Navigation />
-        
+    <>
+      <Navigation />
+      <div className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-        
-        <Footer />
       </div>
+      <Footer />
+    </>
   );
 }
 
